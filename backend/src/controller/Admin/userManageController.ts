52658ea0ba9588b2be6e27b2cb36d8dcf87bff2d
Tran Duc Minh <minhsoc5@gmail.com>
2025-10-22 10:32:40 +0700

    user table

--- backend/src/controller/Admin/userManageController.ts
+++ backend/src/controller/Admin/userManageController.ts
@@ -1,8 +1,21 @@
 import type { Request, Response } from "express"
+import userManageService from "../../service/Admin/userManageService.js";
 
 const userManageController = {
     getAllUser: async (req: Request, res: Response) => {
-
+        try {
+            const result = await userManageService.getAllUser();
+            if (result.success) {
+                return res.status(200).json(result);
+            }
+            return res.status(500).json(result);
+        }
+        catch (error) {
+            return res.status(500).json({
+                success: false,
+                error: error
+            })
+        }
     }
 }
 
